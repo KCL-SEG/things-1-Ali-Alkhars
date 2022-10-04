741899f244b0831dbf8608d285d4aa25b862f251
Ali Alkhars <a-khars@hotmail.com>
2022-10-04 23:33:39 +0100

    Add a basic things model and test

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,12 @@
+from django.forms import ValidationError
 from django.test import TestCase
+from things.models import Things
 
-# Create your tests here.
+class ThingModelTestCase(TestCase):
+    def test_thing_can_be_created(self):
+        thing = Things(name='Key', description='used to open doors', quantity=1)
+
+        try:
+            thing.full_clean()
+        except(ValidationError):
+            self.fail("Cannot create user")
