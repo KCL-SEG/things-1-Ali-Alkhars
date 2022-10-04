3311dc47149c64cfe4d9158d554c8dec74c4b5a2
Ali Alkhars <a-khars@hotmail.com>
2022-10-05 00:20:37 +0100

    Do database migration and fix blank bug

--- things/models.py
+++ things/models.py
@@ -5,13 +5,15 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(Model):
     name = models.CharField(
         max_length = 30,
-        # unique=True,
-        blank = False
+        unique=True,
+        blank = True
     )
     description = models.CharField(
+        unique=False,
         max_length = 120
     )
     quantity = models.IntegerField(
+        unique=False,
         validators = [
             MinValueValidator(limit_value=0, message='You have entered a value below the minimum'),
             MaxValueValidator(limit_value=100, message='You have entered a value above the maximum')
