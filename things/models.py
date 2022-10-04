7a739497075cf75606eae9b8cca3451d8e6a77a2
Ali Alkhars <a-khars@hotmail.com>
2022-10-04 23:56:18 +0100

    Refine Thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(Model):
-    name = models.CharField(max_length = 30)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False
+    )
+    description = models.CharField(
+        max_length = 120
+    )
+    quantity = models.IntegerField(
+        validators = [
+            MinValueValidator(limit_value=0, message='You have entered a value below the minimum'),
+            MaxValueValidator(limit_value=100, message='You have entered a value above the maximum')
+        ]
+    )
--- things/tests.py
+++ things/tests.py
@@ -3,10 +3,34 @@ from django.test import TestCase
 from things.models import Thing
 
 class ThingModelTestCase(TestCase):
-    def test_thing_can_be_created(self):
-        thing = Thing(name='Key', description='used to open doors', quantity=1)
+    def setUp(self):
+        self.thing = Thing(name='Key', description='used to open doors', quantity=1)
 
+    def test_thing_can_be_created(self):
+        self._assert_thing_is_valid()
+    
+    def test_accepted_zero_quantity(self):
+        self.thing.quantity = 0
+        self._assert_thing_is_valid()
+
+    def test_accepted_hundred_quantity(self):
+        self.thing.quantity = 100
+        self._assert_thing_is_valid()
+    
+    def test_cannot_create_negative_quantity(self):
+        self.thing.quantity = -1
+        self._assert_thing_is_invalid()
+    
+    def test_cannot_create_above_hundred_quantity(self):
+        self.thing.quantity = 101
+        self._assert_thing_is_invalid()
+    
+    def _assert_thing_is_valid(self):
         try:
-            thing.full_clean()
+            self.thing.full_clean()
         except(ValidationError):
             self.fail("Cannot create user")
+    
+    def _assert_thing_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.thing.full_clean()
